2779f2bd1401f7dd534f8a3a4657fe3d1370169d
Jisha Salariya <jsalariya2008@gmail.com>
2026-04-17 20:28:35 +0530

    fixed db config

--- app.py
+++ app.py
@@ -26,11 +26,11 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'webp'}
 
 
 # ── MySQL ──────────────────────────────────────────────────────────
-app.config['MYSQL_HOST'] = os.getenv("mysql-3baab388-algo20wear.e.aivencloud.com")
-app.config['MYSQL_USER'] = os.getenv("avnadmin")
+app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
+app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
 app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
-app.config['MYSQL_DB'] = os.getenv("algo_store")
-app.config['MYSQL_PORT'] = int(os.getenv("MYSQL_PORT", 26131))
+app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
+app.config['MYSQL_PORT'] = int(os.getenv("MYSQL_PORT", 3306))
 app.config['MYSQL_CURSORCLASS'] = os.getenv("DictCursor")
 
 app.config['MYSQL_SSL']  = {'REQUIRED': None}
